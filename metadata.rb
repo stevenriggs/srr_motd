2525f3e42d532b6a5e60a373441148d944287fa7
Steven Riggs <steven.riggs@icloud.com>
2015-10-20 22:46:44 -0400

    Updated license

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'srr_motd'
 maintainer       'Steven Riggs'
 maintainer_email 'steven.riggs@icloud.com'
-license          'All rights reserved'
+license          'Apache License Version 2.0'
 description      'Installs/Configures srr_motd'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.0.0'
